c207b1897a937c23c662d97a5439c15b2f9765fe
ddunchev <danchodunchev@gmail.com>
2017-04-23 05:53:39 +0300

    Largest N Elements

---
+++ LargestNelements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LergestNelements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var input = int.Parse(Console.ReadLine());
+
+            var orderedList = myList.OrderByDescending(x => x);
+
+            foreach (var item in orderedList)
+            {
+                Console.Write(item + " ");
+                input--;
+                if (input == 0) break;
+            }
+        }
+    }
+}
