d866fe29e2c72b8c6f9d21b601f6ae251fc82e99
ddunchev <danchodunchev@gmail.com>
2017-04-23 05:47:56 +0300

    Writing code in the project

--- SortArrayUsingBubbleSort/Program.cs
+++ SortArrayUsingBubbleSort/Program.cs
@@ -10,6 +10,32 @@ namespace SortArrayUsingBubbleSort
     {
         static void Main(string[] args)
         {
+            int[] myArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            bool swapped;
+
+            do
+            {
+                swapped = false;
+
+                for (int i = 0; i < myArray.Length - 1; i++)
+                {
+                    if (myArray[i] > myArray[i + 1])
+                    {
+                        Swap(myArray, i, i + 1);
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+
+            Console.WriteLine(String.Join(" ", myArray));
+        }
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            int temp = 0;
+            temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
         }
     }
 }
